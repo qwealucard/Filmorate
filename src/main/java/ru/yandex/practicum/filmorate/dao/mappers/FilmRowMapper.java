d25275cb89b3a242b3456d900ce0a5279bdc49a8
Boris <Zelyanin>
2025-01-15 19:54:24 +0300

    create a new function

--- src/main/java/ru/yandex/practicum/filmorate/dao/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/mappers/FilmRowMapper.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-@Component
+@Component("filmRowMapper")
 public class FilmRowMapper implements RowMapper<Film> {
 
     @Override
