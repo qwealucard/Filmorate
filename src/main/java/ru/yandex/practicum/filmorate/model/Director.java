7ed3eadb4b5644896ca2c87eb56b5affbb83012d
Boris <Zelyanin>
2025-01-16 19:48:26 +0300

    create a new function

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,8 +19,9 @@ public class Director {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotNull
-    @Size(min = 1, max = 255, message = "Наименование автора не может быть пустой строкой и должно быть не длиннее 255 символов")
+    @NotNull(message = "Наименование автора не может быть null")
+    @Size(min = 1, max = 255, message = "Наименование автора должно быть не длиннее 255 символов")
+    @Pattern(regexp = ".*\\S.*", message = "Наименование автора не может быть пустым или состоять только из пробелов")
     @Column(nullable = false, unique = true, length = 255)
     private String name;
 }
